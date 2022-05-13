ce5adf351d64077f63ca873e99d4284914882a6a
Henrique Mochida <hmochida@student.42sp.org.br>
2022-05-13 17:31:05 -0300

    make git

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: hmochida <hmochida@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 02:40:10 by hmochida          #+#    #+#             */
-/*   Updated: 2022/05/13 15:38:18 by hmochida         ###   ########.fr       */
+/*   Updated: 2022/05/13 17:31:01 by hmochida         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ int main (int argc, char *argv[])
 	t_data *data;
 	data = malloc (sizeof(t_data));
 
+//esse bloco printa todos os argumentos.
 	int i = 0;
 	while (i < argc)
 	{
@@ -28,11 +29,10 @@ int main (int argc, char *argv[])
 	data->child_pid[0] = create_child();
 	if (!data->child_pid[0])
 	{
-		/*child1 vai abrir
 	/* 	código para o child1 */
 		printf ("data->child_pid[0] = %d ;\n", data->child_pid[0]);
 		printf ("this is child 1\n");
-		exit(UTIL_NOT_FOUND);
+		exit(UTIL_NOT_FOUND); //if command is not found, gtfo of the forked process.
 	}
 
 	waitpid(data->child_pid[0], &data->status, 0); // wait for 1st child shit to be complete.
@@ -43,7 +43,7 @@ int main (int argc, char *argv[])
 	/* 	código para o child1 */
 		printf ("data->child_pid[1] = %d ;\n", data->child_pid[1]);
 		printf ("this is child 2\n");
-		exit(UTIL_NOT_FOUND);
+		exit(UTIL_NOT_FOUND); //if command is not found, gtfo of the forked process.
 	}
 	waitpid(data->child_pid[1], &data->status, 0); // wait for 2nd child shit to be complete.
 
